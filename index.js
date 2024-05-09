67f34d0b767e446510b0f9a8ceda67c12d021a43
Fernando7 <fernandomullerc@gmail.com>
2024-05-08 21:18:22 -0300

    Corrigindo erro no HTML

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('express');
+const cors = require('cors');
 const eventos = require('./eventos.json'); // Importação dos dados de eventos.
 const path = require('path');
 
--- public/app.js
+++ public/app.js
@@ -1,12 +1,14 @@
 fetch('http://localhost:8080/eventos') //isso é uma função para exibir os dados dos eventos
-.then(response => response.json())
-.then(data => { //manipulando os dados recebidos
-const eventosList1 = document.getElementById('eventos__gastronomicos')
-const eventosList2 = document.getElementById('eventos__tecnologicos')
-const eventosList3 = document.getElementById('eventos__aventuras')
-const eventosList4 = document.getElementById('eventos__moda')
-data.forEach(evento =>{ //iterando sobre os eventos recebidos
-const eventoDiv = document.createElement('div');
-eventoDiv
-})
-})
+    .then(response => response.json())
+    .then(data => { //manipulando os dados recebidos
+        const eventosList1 = document.getElementById('eventos__gastronomicos')
+        data.forEach(evento => { //iterando sobre os eventos recebidos
+            const eventoDiv = document.createElement('div');
+            eventoDiv.innerHTML = `
+            <h2>${evento.nome}</h2>
+            <p>${evento.idade}, ${evento.sexo}</p>
+            `;
+
+            eventosList1.appendChild(eventoDiv);
+        })
+    }).catch(error => console.error('Erro ao carregar os filmes:', error));
