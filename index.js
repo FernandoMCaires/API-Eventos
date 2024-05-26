df58264e2859375d994a4ed8067bd903ab4eeddc
LucaasAndrade <lucasmarcelodeandradesilva@gmail.com>
2024-05-26 14:14:40 -0300

    API - Busca por nome OK

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const eventos = require('./eventos.json'); // Importação dos dados de eventos.
 const path = require('path');
+const { log } = require('console');
 
 const app = express();
 const PORT = 8080;
@@ -17,6 +18,35 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/eventos', (req, res)=>{
     res.json(eventos); // Retorna os dados dos filmes como uma resposta JSON
 });
+
+
+app.get('/eventos/busca/:nome', (req, resp) => {
+    try{
+        const nome = req.params.nome.toLowerCase();
+        let dados = eventos;
+        let busca = [];
+        let nomeEvento = '';
+
+        for(let i = 0; i < dados.length; i++){
+
+            nomeEvento = dados[i].nome;
+            nomeEvento = nomeEvento.split(' ');
+
+            for(let j = 0; j < nomeEvento.length; j++){
+                if(nomeEvento[j].toLowerCase() == nome){
+                    busca.push(dados[i]);
+                }
+            }
+        }
+
+        resp.send(busca);
+
+    }catch (err) {
+        resp.status(400).send({
+            erro: err.message
+        })
+    }
+})
 
 //Inicialização do servidor Express:
 app.listen(PORT,()=>{
--- public/app.js
+++ public/app.js
@@ -1,14 +1,49 @@
-fetch('http://localhost:8080/eventos') //isso é uma função para exibir os dados dos eventos
-    .then(response => response.json())
-    .then(data => { //manipulando os dados recebidos
-        const eventosList1 = document.getElementById('eventos__gastronomicos')
-        data.forEach(evento => { //iterando sobre os eventos recebidos
-            const eventoDiv = document.createElement('div');
-            eventoDiv.innerHTML = `
-            <h2>${evento.nome}</h2>
-            <p>${evento.idade}, ${evento.sexo}</p>
-            `;
 
-            eventosList1.appendChild(eventoDiv);
-        })
-    }).catch(error => console.error('Erro ao carregar os filmes:', error));
+const textBusca = document.getElementById('buscaNome');
+
+function Main(){    
+    fetch('http://localhost:8080/eventos') //isso é uma função para exibir os dados dos eventos
+        .then(response => response.json())
+        .then(data => { //manipulando os dados recebidos
+            const eventosList1 = document.getElementById('eventos__gastronomicos')
+            data.forEach(evento => { //iterando sobre os eventos recebidos
+                const eventoDiv = document.createElement('div');
+                eventoDiv.innerHTML = `
+                <h4 class='card_title'> ${evento.nome} </h4>
+                <div class='card_info_all>
+                    <p class='card_info'>${evento.categoria}</p>
+                    <p class='card_info_classificacao'>${evento.classificação}</p>
+                    <p class='card_info'>${evento.data}</p>
+                </div>
+                <div class='card_info_desc'>
+                    <p>${evento.descrição} </p>
+                </div>
+                <div class='card_info_end'>
+                    <p>${evento.endereço}</p>
+                </div>
+                <a> Ir para pagina >>> </a>
+                `;
+    
+                eventosList1.appendChild(eventoDiv);
+            })
+        }).catch(error => console.error('Erro ao carregar os filmes:', error));
+}    
+
+
+
+function buscar(){
+    console.log(textBusca.value())
+
+    btnBusca.addEventListener('click', (e) => {
+        e.preventDefault();
+        let data = textBusca.value;
+
+        if(data == ''){
+            window.alert('Digite o nome de algum evento para pesquisar!');
+        }
+    })
+}
+
+
+
+Main();
