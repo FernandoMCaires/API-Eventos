91c8006805519489b9c29755f25a52acfed8d90d
LucaasAndrade <lucasmarcelodeandradesilva@gmail.com>
2024-05-26 16:09:49 -0300

    API: BUSCA POR ID OK

--- index.js
+++ index.js
@@ -19,19 +19,26 @@ app.get('/eventos', (req, res)=>{
     res.json(eventos); // Retorna os dados dos filmes como uma resposta JSON
 });
 
-
+// ENDPOINT PARA BUSCA POR NOME
 app.get('/eventos/busca/:nome', (req, resp) => {
+    // TRATAEMNTO DE ERRO
     try{
+        // DECLARANDO VARIÁVEIS 
+        // PEGANDO O NOME E COLOCANDO EM MINUSCULO
         const nome = req.params.nome.toLowerCase();
         let dados = eventos;
         let busca = [];
         let nomeEvento = '';
 
+
+        // LAÇO DE REPETIÇÃO PARA TODOS EVENTOS
         for(let i = 0; i < dados.length; i++){
 
+            // PEGANDO O NOME DO EVENTO E SEPERANDO EM INDÍCES DE ARRAY
             nomeEvento = dados[i].nome;
             nomeEvento = nomeEvento.split(' ');
 
+            // COMPARANDO COM O ARRAY, SE TIVER O NOME DE BUSCA, IRÁ ADICIONAR PARA O ARRAY "BUSCA"
             for(let j = 0; j < nomeEvento.length; j++){
                 if(nomeEvento[j].toLowerCase() == nome){
                     busca.push(dados[i]);
@@ -41,12 +48,39 @@ app.get('/eventos/busca/:nome', (req, resp) => {
 
         resp.send(busca);
 
-    }catch (err) {
+    } catch (err) {
         resp.status(400).send({
             erro: err.message
         })
     }
 })
+
+
+
+// BUSCA POR ID
+
+app.get('/eventos/id/:id', (req, resp) => {
+    try{
+        const {id} = req.params;
+        let dados = eventos;
+
+        dados.forEach(item => {
+            if(Number(item.id) == Number(id)){
+                console.log(Number(item.id));
+                resp.json(item);
+            }
+        })
+        
+        throw new Error(`Não foi encontrado nenhum item com o ID ${id}`)
+
+    }catch (err){
+        resp.status(400).send({
+            erro: err.message
+        })
+    }
+})
+
+
 
 //Inicialização do servidor Express:
 app.listen(PORT,()=>{
