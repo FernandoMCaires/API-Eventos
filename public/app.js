ac123463751358e2251b9b48e0515d1ea35293e8
Pedro Joaquim Araujo <pedroa990@gmail.com>
2024-05-27 17:42:02 -0300

    add style com bootstrap aos cards

--- public/app.js
+++ public/app.js
@@ -1,49 +1,58 @@
+const textBusca = document.getElementById("buscaNome");
 
-const textBusca = document.getElementById('buscaNome');
+function Main() {
+  function limitarDescricao(descricao, limite) {
+    return descricao.length > limite
+      ? descricao.substring(0, limite) + "..."
+      : descricao;
+  }
+  fetch("http://localhost:8080/eventos") //isso é uma função para exibir os dados dos eventos
+    .then((response) => response.json())
+    .then((data) => {
+      //manipulando os dados recebidos
+      const eventosList1 = document.getElementById("eventos__gastronomicos");
+      data.forEach((evento) => {
+        //iterando sobre os eventos recebidos
+        const eventoDiv = document.createElement("div");
 
-function Main(){    
-    fetch('http://localhost:8080/eventos') //isso é uma função para exibir os dados dos eventos
-        .then(response => response.json())
-        .then(data => { //manipulando os dados recebidos
-            const eventosList1 = document.getElementById('eventos__gastronomicos')
-            data.forEach(evento => { //iterando sobre os eventos recebidos
-                const eventoDiv = document.createElement('div');
-                eventoDiv.innerHTML = `
-                <h4 class='card_title'> ${evento.nome} </h4>
-                <div class='card_info_all>
-                    <p class='card_info'>${evento.categoria}</p>
-                    <p class='card_info_classificacao'>${evento.classificação}</p>
-                    <p class='card_info'>${evento.data}</p>
-                </div>
-                <div class='card_info_desc'>
-                    <p>${evento.descrição} </p>
-                </div>
-                <div class='card_info_end'>
-                    <p>${evento.endereço}</p>
-                </div>
-                <a> Ir para pagina >>> </a>
-                `;
-    
-                eventosList1.appendChild(eventoDiv);
-            })
-        }).catch(error => console.error('Erro ao carregar os filmes:', error));
-}    
+        eventoDiv.innerHTML = `
+        <div class="card" style="
+        width: 35rem;
+        height: 400px;
+        ">
+        
+        <div class="card-body d-flex flex-column">
+          <h4 class="card-title"> ${evento.nome} </h4>
+          <p class="card-subtitle">${evento.categoria} // ${
+          evento.classificação
+        }</p>
+          <p class="card-text text-decoration-underline my-3 ">${
+            evento.data
+          }</p>
+          <p class="card-text">${limitarDescricao(evento.descrição, 250)}</p>
+          <p class="card-text fw-bold text-secondary">${evento.endereço}</p>
+          <a href="#" class="btn btn-primary mt-auto w-50">Ir para pagina <i class="bi bi-arrow-right"></i></a>
+        </div>
+      </div>
+      `;
 
-
-
-function buscar(){
-    console.log(textBusca.value())
-
-    btnBusca.addEventListener('click', (e) => {
-        e.preventDefault();
-        let data = textBusca.value;
-
-        if(data == ''){
-            window.alert('Digite o nome de algum evento para pesquisar!');
-        }
+        eventosList1.appendChild(eventoDiv);
+      });
     })
+    .catch((error) => console.error("Erro ao carregar os filmes:", error));
 }
 
+function buscar() {
+  console.log(textBusca.value());
 
+  btnBusca.addEventListener("click", (e) => {
+    e.preventDefault();
+    let data = textBusca.value;
+
+    if (data == "") {
+      window.alert("Digite o nome de algum evento para pesquisar!");
+    }
+  });
+}
 
 Main();
